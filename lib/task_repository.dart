e28bcf63cb07cb086343cbeb802b5f0fe6cc72b8
Stason5111 <144026365+Stason5111@users.noreply.github.com>
2024-01-22 22:10:15 +1000

    Add files via upload

---
+++ lib/task_repository.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'main.dart';
+
+
+
+class TaskRepository {
+  Future<Brightness> loadBrightness() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String savedBrightness = prefs.getString('brightness') ?? 'light';
+    return savedBrightness == 'light' ? Brightness.light : Brightness.dark;
+  }
+
+  Future<void> saveBrightness(Brightness brightness) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setString('brightness', brightness == Brightness.light ? 'light' : 'dark');
+  }
+
+  Future<List<Task>> loadTasks() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    List<String> taskList = prefs.getStringList('tasks') ?? [];
+    return taskList.map((task) => Task.fromJson(task)).toList();
+  }
+
+  Future<void> saveTasks(List<Task> tasks) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    List<String> taskList = tasks.map((task) => task.toJson()).toList();
+    await prefs.setStringList('tasks', taskList);
+  }
+}
